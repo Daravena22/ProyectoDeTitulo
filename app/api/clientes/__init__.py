4d300e8ce9fcc95d4660a0459ed8cd68e73dfc87
Daravena22 <80468074+Daravena22@users.noreply.github.com>
2023-09-12 00:08:06 -0300

    Vista Clientes
    Se modifica y corrige la vista clientes, además de agregar el metodo Eliminar Cliente

--- app/api/clientes/__init__.py
+++ app/api/clientes/__init__.py
@@ -36,9 +36,41 @@ def listar_clientes():
     pagina_lenght = int(request.args.get("length"))
     start = int(request.args.get("start"))
     pagina_index = int(start/pagina_lenght+1)
+    draw = int(request.args.get("draw"))
     query = db.session.query(Cliente.id,Cliente.rut,Cliente.apellido,Cliente.nombre,Cliente.telefono,Cliente.direccion).paginate(page=pagina_index,per_page=pagina_lenght,error_out=False)
     rows=query.items
     data=SqlUtils.rows_to_dict(rows)
-    return jsonify({"data": data, "recordsTotal": query.total,"draw":1,"recordsFiltered":query.total})
+    return jsonify({"data": data, "recordsTotal": query.total,"draw":draw,"recordsFiltered":query.total})
+
+@api_clientes.route("/eliminar", methods=["DELETE"])
+@login_required
+def eliminar_cliente():
+    valores = request.get_json()
+    id = valores["id"]
+    cliente = db.session.query(Cliente).filter(Cliente.id==id).first()
+    db.session.delete(cliente)
+    db.session.commit()
+    return jsonify({"status":'ok'}), 200
 
 
+@api_clientes.route("/editar", methods=["UPDATE"])
+@login_required
+def editar_cliente():
+    valores = request.get_json()
+    rut= valores['rut']
+    apellido = valores['apellido']
+    nombre = valores['nombre']
+    telefono = valores['telefono']
+    direccion = valores['direccion']
+
+    cliente = Cliente()
+    cliente.rut = rut
+    cliente.apellido = apellido
+    cliente.nombre = nombre
+    cliente.telefono = telefono
+    cliente.direccion = direccion
+
+    db.session.add(cliente)
+    db.session.commit()
+    return jsonify({"status":'ok'}), 200
+  
--- app/web/clientes/static/clientes.js
+++ app/web/clientes/static/clientes.js
@@ -5,6 +5,7 @@ const agregarCliente = async () => {
     nombre = document.getElementById('nombre').value
     telefono = document.getElementById('telefono').value
     direccion = document.getElementById('direccion').value
+    
     const response = await fetch('/api/clientes/agregar', {
       method: 'PUT',
       body:JSON.stringify({rut:rut,apellido:apellido,nombre:nombre,telefono:telefono,direccion:direccion}), // string or object
@@ -13,6 +14,9 @@ const agregarCliente = async () => {
       }
     });
     const myJson = await response.json(); 
+    $('#rut').val('')
+    $('#datos_clientes').DataTable().ajax.reload();
+    $('#AgregarClienteModal').modal('toggle')
     //extract JSON from the http response
     // do something with myJson
   }
@@ -32,9 +36,64 @@ const agregarCliente = async () => {
           { data: 'apellido', title: 'APELLIDO' },
           { data: 'nombre', title: 'NOMBRE' },
           { data: 'telefono', title: 'TELEFONO' },
-          { data: 'direccion', title: 'DIRECCION' }
+          { data: 'direccion', title: 'DIRECCION' },
+          {
+            data: null, title: "ACCIONES",
+            render: function (data, type, row) {
+              return '<button type="button" class="btn btn-danger eliminar-btn" onclick="eliminarCliente(' + row.id + ')">Eliminar</button>' +
+                     '<button type="button" class="btn btn-primary editar-btn" onclick="editarCliente(' + row.id + ')">Editar</button>';
+            }
+          }
+          
             // Configura tus columnas aquí
         ]
     });
 });
+
+
+const eliminarCliente = async (id_cliente) => { 
+  const response = await fetch('/api/clientes/eliminar', {
+    method: 'DELETE',
+    body:JSON.stringify({id:id_cliente}), // string or object
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const myJson = await response.json(); 
+  $('#datos_clientes').DataTable().ajax.reload();
+  //extract JSON from the http response
+  // do something with myJson
+}
+
+const editarCliente = async (id_cliente) => {
+  // Recopila los datos del formulario de edición aquí
+  const rut = document.getElementById('editar_rut').value;
+  const apellido = document.getElementById('editar_apellido').value;
+  const nombre = document.getElementById('editar_nombre').value;
+  const telefono = document.getElementById('editar_telefono').value;
+  const direccion = document.getElementById('editar_direccion').value;
+
+  const data = {
+    id: id_cliente,
+    rut: rut,
+    apellido: apellido,
+    nombre: nombre,
+    telefono: telefono,
+    direccion: direccion
+  };
+
+  const response = await fetch('/api/clientes/editar', {
+    method: 'UPDATE',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  
+  const myJson = await response.json();
+  $('#datos_clientes').DataTable().ajax.reload();
+  // Extrae JSON de la respuesta HTTP
+  // Haz algo con myJson si es necesario
+}
+
 
